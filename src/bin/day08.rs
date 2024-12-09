67a101116a1da3f0289d42c487c2f8949a6e6fea
rfdolan <dolanraymond416@gmail.com>
2024-12-09 10:16:28 -0800

    day08 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,6 +1,7 @@
 use std::vec::Vec;
 use std::ops::{Sub,Add};
 use std::collections::{HashMap, HashSet};
+use std::collections::hash_map::Entry;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Point {
@@ -35,20 +36,33 @@ fn main(){
   println!("Part 2: {}", solve_part2(&vec));
 }
 
+fn get_antennas(input: &Vec<String>) -> HashMap<char, Vec<Point>> {
+  let mut antennas: HashMap<char, Vec<Point>> = HashMap::new();
+  for (y, line) in input.iter().enumerate() {
+    for (x, character) in line.chars().enumerate() {
+      if character == '.' { continue;}
+      let point = Point{x: x as i32, y: y as i32};
+      match antennas.entry(character) {
+        Entry::Occupied(mut entry) => entry.get_mut().push(point),
+        Entry::Vacant(entry) => {
+            entry.insert(vec![point]);
+        }
+      }
+    }
+  }
+  antennas
+}
+
 fn create_antinodes(antennas: &Vec<Point>, anodes: &mut HashSet<Point>, gridsize: (i32, i32))  {
-  for a1 in 0..antennas.len() {
-    let point1 = antennas[a1];
-    for a2 in a1..antennas.len() {
-      let point2= antennas[a2];
+  for (i1, &point1) in antennas.iter().enumerate(){
+    for &point2 in &antennas[i1+1..]{
       let diff = point1 - point2;
       let spots = vec![point1+diff, point2+diff, point1-diff, point2-diff];
       for spot in spots {
         if spot != point1 && spot != point2 &&
-        !anodes.contains(&spot) && 
         (0..gridsize.0).contains(&spot.x) &&
         (0..gridsize.1).contains(&spot.y) {
           anodes.insert(spot);
-          //println!("{:?}", spot);
         }
       }
     }
@@ -57,34 +71,45 @@ fn create_antinodes(antennas: &Vec<Point>, anodes: &mut HashSet<Point>, gridsize
 
 // Solution for part 1
 fn solve_part1(input: &Vec<String>) -> i32 {
-  let mut antennas: HashMap<char, Vec<Point>> = HashMap::new();
-  let mut is_occupied = HashSet::new();
+  let antennas = get_antennas(input);
   let xsize = input[0].len() as i32;
   let ysize = input.len() as i32;
-  for (y, line) in input.iter().enumerate() {
-    for (x, character) in line.chars().enumerate() {
-      if character == '.' { continue;}
-      let point = Point{x: x as i32, y: y as i32};
-      match antennas.get_mut(&character) {
-        Some(val) => val.push(point),
-        None => {
-          antennas.insert(character, vec![point]);
-          ()}
-      }
-      is_occupied.insert(point);
-    }
-  }
   let mut anodes = HashSet::new();
-  for antenna_type in antennas {
-    create_antinodes(&antenna_type.1, &mut anodes, (xsize, ysize));
-
+  for (_label, locations) in antennas {
+    create_antinodes(&locations, &mut anodes, (xsize, ysize));
   }
   anodes.len() as i32
 }
 
+fn create_antinodes2(antennas: &Vec<Point>, anodes: &mut HashSet<Point>, gridsize: (i32, i32))  {
+  for (i1, &point1) in antennas.iter().enumerate(){
+    for &point2 in &antennas[i1+1..]{
+      let diff = point1 - point2;
+      let mut it = point1-diff;
+      while (0..gridsize.0).contains(&it.x) && (0..gridsize.1).contains(&it.y) {
+        anodes.insert(it);
+        it = it - diff;
+      }
+      it = point1+diff;
+      while (0..gridsize.0).contains(&it.x) && (0..gridsize.1).contains(&it.y) {
+        anodes.insert(it);
+        it = it + diff;
+      }
+      anodes.insert(point1);
+    }
+  }
+}
+
 // Solution for part 2
-fn solve_part2(_input: &Vec<String>) -> i32 {
-  0
+fn solve_part2(input: &Vec<String>) -> i32 {
+  let antennas = get_antennas(input);
+  let xsize = input[0].len() as i32;
+  let ysize = input.len() as i32;
+  let mut anodes = HashSet::new();
+  for (_, locations) in antennas {
+    create_antinodes2(&locations, &mut anodes, (xsize, ysize));
+  }
+  anodes.len() as i32
 }
 
 #[cfg(test)]
@@ -93,5 +118,6 @@ mod day08_tests {
   #[test]
   fn test() {
     assert_eq!(14, solve_part1(&inp::parse_file("test_inputs/day08_test.txt")));
+    assert_eq!(34, solve_part2(&inp::parse_file("test_inputs/day08_test.txt")));
   }
 }
